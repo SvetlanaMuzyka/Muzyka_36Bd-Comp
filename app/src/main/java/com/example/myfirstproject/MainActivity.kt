4fa7be90793c40b4c18eba4558e93b64eae670f6
svitlanamuzika <muzikasvitlana4@gmail.com>
2023-02-21 17:57:19 +0200

    Third commit

--- app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.myfirstproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,4 +10,5 @@ class MainActivity : AppCompatActivity() {
 
         //dfsl;;lfdks;ldsf
     }
+    //test
 }
